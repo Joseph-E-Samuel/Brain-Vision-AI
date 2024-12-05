ae46d7142c15c5036cd2d750313c22558cf7a191
Joseph Samuel <59376205+YourAverageDev@users.noreply.github.com>
2024-12-05 00:08:52 -0800

    attempting to implement css properly

--- backend/brain_cancer_identification/brain_cancer/urls.py
+++
@@ -1,7 +0,0 @@
-
-from django.urls import path
-from .views import BrainCancerIdentificationAPIView
-
-urlpatterns = [
-    path('images/', BrainCancerIdentificationAPIView.as_view(), name='brain-cancer-identification-api-view'),
-]
